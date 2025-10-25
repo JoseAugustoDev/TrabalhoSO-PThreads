197dd8e8f5bd3c8cf65e16dcda213c99c9319775
Jose Augusto Costa <75041668+JoseAugustoDev@users.noreply.github.com>
2025-10-25 20:50:42 -0300

    Alocação de Matriz de acordo com a forma que a Apostila manda fazer; Codigo Serial de busca e contagem de numeros primos funcionando

--- TrabalhoSO/Main.c
+++ TrabalhoSO/Main.c
@@ -6,20 +6,116 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-void* threadFunc(void*);
+#include <math.h>
+#include <time.h>
+
+#define INTERVALO_MATRIZ 31999
+#define TAMANHO_MATRIZ 10000 // 22000 foi o maior que consegui rodar na minha maquina
+#define TAMANHO_MACRO_BLOCO 3
+
+int** matriz;
+int contador = 0;
+
+int** alocar_matriz(int linhas, int colunas) {
+	int** matriz;
+	int i;
+
+	if (linhas < 1 || colunas < 1) {
+		printf("Tamanho invalido para a matriz\n");
+		return (NULL);
+	}
+
+	matriz = calloc(linhas, sizeof(int*));
+
+	if (matriz == NULL) {
+		printf("Erro ao alocar memoria para a matriz\n");
+		return (NULL);
+	}
+
+	for (i = 0; i < linhas; i++)
+	{
+		matriz[i] = calloc(colunas, sizeof(int));
+		if (matriz[i] == NULL) {
+			printf("Erro ao alocar memoria para a matriz\n");
+			return (NULL);
+		}
+	}
+
+	return (matriz);
+}
+
+int **liberar_matriz(int linhas, int colunas, int** matriz) {
+	int i;
+	if (matriz == NULL) {
+		return (NULL);
+	}
+
+	if (linhas < 1 || colunas < 1) {
+		printf("Erro: Parametro Invalido\n");
+		return (matriz);
+	}
+
+	for (i = 0; i < linhas; i++) free(matriz[i]);
+	free(matriz);
+
+	return (NULL);
+}
+
 int main(int argc, char* argv[]) {
-	pthread_t thread;
-	if (pthread_create(&thread, NULL, threadFunc, NULL) != 0) {
-		perror("Pthread_create falhou!");
-		exit(1);
+
+	int** matriz;
+
+	matriz = alocar_matriz(TAMANHO_MATRIZ, TAMANHO_MATRIZ);
+
+	srand(time(NULL));
+
+	clock_t start_t, end_t;
+	double total_t;
+
+	for (int i = 0; i < TAMANHO_MATRIZ; i++) {
+		for (int j = 0; j < TAMANHO_MATRIZ; j++) {
+			matriz[i][j] = rand() % INTERVALO_MATRIZ + 1;
+		}
 	}
-	if (pthread_join(thread, NULL) != 0) {
-		perror("Pthread_join falhou!");
-		exit(1);
+
+	
+	start_t = clock();
+	for (int i = 0; i < TAMANHO_MATRIZ; i++)
+	{
+		for (int j = 0; j < TAMANHO_MATRIZ; j++)
+		{
+			if (ehPrimo(matriz[i][j])) {
+				contador++;
+			}
+		}
+		
 	}
-	printf("Print do Main");
+	end_t = clock();
+
+	total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+
+	printf("Total de numeros primos: %d\n", contador);
+	printf("Tempo CPU: %f\n", total_t);
+
+	matriz = liberar_matriz(TAMANHO_MATRIZ, TAMANHO_MATRIZ, matriz);
+
 	return 0;
 }
-void* threadFunc(void* nenhum) {
-	printf("Print da Thread\n");
+
+int ehPrimo(int n) {
+
+	if (n < 2) return 0;
+	if (n == 2) return 1;
+	if (n % 2 == 0) return 0;
+
+	int sqrt_n = sqrt(n);
+
+	for (int i = 3; i <= sqrt_n; i+=2)
+	{
+		if	(n % i == 0) {
+			return 0;
+		}
+	}
+
+	return 1;
 }
